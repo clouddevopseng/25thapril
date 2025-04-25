345aa9076acee0ef6f816c9e5aed2f7ac41f7788
venkat <venkat@tcs.com>
2025-04-25 16:36:14 +0530

    mypro

---
+++ src/main/java/icicibank/pernaldepoart/PernaldepoartApplication.java
@@ -0,0 +1,13 @@
+package icicibank.pernaldepoart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PernaldepoartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PernaldepoartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/icicibank/pernaldepoart/PernaldepoartApplicationTests.java
@@ -0,0 +1,13 @@
+package icicibank.pernaldepoart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PernaldepoartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
